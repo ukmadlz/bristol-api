0b2231ccd3658e589c265954824e0028cee3e53c
Mike Elsmore <mike@elsmore.me>
2016-01-23 16:30:22 +0000

    Hack in some dynamic stuff

--- index.js
+++ index.js
@@ -47,28 +47,35 @@ server.route({
 
       urbanThings.getStation(fromName, function(error, response, body){
         if (error || !body.success) return false;
-        // var from = {
-        //   lat: body.data.placePoints[0].lat,
-        //   lng: body.data.placePoints[0].lng
-        // };
-        var from = body.data.placePoints[0].primaryCode;
-        console.log(from);
+        var origin = {
+          lat: body.data.placePoints[0].lat,
+          lng: body.data.placePoints[0].lng
+        };
+        console.log('FROM---',body.data.placePoints[0])
         urbanThings.getStation(toName, function(error, response, body){
           if (error || !body.success) return false;
-          // var to = {
-          //   lat: body.data.placePoints[0].lat,
-          //   lng: body.data.placePoints[0].lng
-          // };
-          var to = body.data.placePoints[0].primaryCode;
-          urbanThings.journeyDirect(from, to, function(error, response, body){
-            console.log(response);
+          var destination = {
+            lat: body.data.placePoints[0].lat,
+            lng: body.data.placePoints[0].lng
+          };
+          console.log('To---',body.data.placePoints[0])
+          var timestamp = (new Date()).toISOString();
+          urbanThings.journeyDirect(origin, destination, timestamp, function(error, response, body){
+            console.log(timestamp);
+            for(var i = 0; i < body.data.journeys.length; i++) {
+              console.log(body.data.journeys[i].departureTime);
+              if(new Date(body.data.journeys[i].departureTime)>new Date(timestamp)) {
+                console.log(body.data.journeys[i]);
+                break;
+              }
+            }
           });
         });
       });
 
       reply({
           "start_destination" : request.params.from,
-          "final_destination" : 'London',
+          "final_destination" : request.params.to,
           "sheduled_time" : (new Date()).toISOString(),
           "actual_time" : (new Date()).toISOString(),
       }).code( 200 );
--- urbanthings.js
+++ urbanthings.js
@@ -34,25 +34,48 @@ module.exports = function(apiKey) {
 
     },
 
-    journeyDirect: function(from, to, callback) {
-      var opts = {
-        // url: this.baseUrl + this.version + '/static/places/List',
-        // method: 'GET',
-        // json: true,
-        // qs: {
-        //   name: name,
-        //   placepointtypes: 102,
-        //   apikey:apiKey
-        // }
-      };
+    journeyDirect: function(from, to, timestamp, callback) {
 
+      var body = {
+        "Origin": {
+          PlacePointType: 102,
+          lat: from.lat,
+          lng: from.lng
+        },
+        "Destination": {
+          PlacePointType: 102,
+          lat: to.lat,
+          lng: to.lng
+        },
+        "DepartureTime": timestamp,
+        "MaximumJourneys": 5,
+        "Options": {
+          "MaximumLegs": 2,
+          "AcceptableVehicleTypes": [
+            9
+          ]
+        }
+      } ;
+      console.log(body);
+      var opts = {
+        url: this.baseUrl + this.version + '/plan/directions',
+        method: 'POST',
+        json: true,
+        qs: {
+          apikey:apiKey
+        },
+        headers: {
+            "Content-Type": "application/json",
+            'Accept': 'application/json'
+        },
+        body: body
+      };
       return request(opts, function(error, response, body) {
         var returnObject = {
           error: error,
           response: response,
           body: body
         };
-
         return callback(error, response, body)
       });
     }
